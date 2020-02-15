760b675e6d5cc6e62709fa5ec8c7ed0489980c19
Jasmine <jasminedhaliwal29@gmail.com>
2020-02-15 23:05:33 +0000

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,8 +1,9 @@
-word = input("Enter a word: ")
-word = word.lower()
-reverse_word = word[::-1]
-
-if word == reverse_word:
-	print(word + " is a palindrome.")
-else:
-	print(word + " is not a palindrome.")
+def palindrome():
+  word = input("Enter a word: ")
+  word = word.lower()
+  reverse_word = word[::-1]
+  
+  if word == reverse_word:
+    print(word + " is a palindrome.")
+  else:
+    print(word + " is not a palindrome.")
