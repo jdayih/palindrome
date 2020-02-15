2344a76ffaf95ed5bd26a7365e77858b660b07d9
Jasmine <jasminedhaliwal29@gmail.com>
2020-02-15 23:03:40 +0000

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,15 +1,8 @@
-Python 3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> word = input("Enter a word: ")
-Enter a word: racecar
->>> word = word.lower()
->>> reverse_word = word[::-1]
->>> if word == reverse_word:
+word = input("Enter a word: ")
+word = word.lower()
+reverse_word = word[::-1]
+
+if word == reverse_word:
 	print(word + " is a palindrome.")
 else:
 	print(word + " is not a palindrome.")
-
-	
-racecar is a palindrome.
->>> 
